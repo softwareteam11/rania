92fcb1118d3e6540c52926dab3e59ade695b9799
Menna33 <mennalotfy33@gmail.com>
2016-04-23 12:36:18 -0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -3,10 +3,12 @@
 error_reporting(E_PARSE );
 require_once("diet.php");
 require_once("bodya.php");
+require_once('Person.php');
 
 
 class controller {
 	private static  $_instance=null;
+	private $person=null;
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public static  function get_instance_controller  (    ){
 		
@@ -35,5 +37,31 @@ class controller {
 	$results = diet::diet_schedule();
 	return $results;
 	}
+	public function CalculateWeight ($var)
+	{
+		switch ($_POST['gender']) {
+    case "male":
+        $person=new male;
+        break;
+    case "female":
+        $person=new female;
+break;}
+
+switch ($_POST['cm']) {
+    case 1:
+        $num=$person->CalculateByCMs($_POST['Height']);
+		/*CalculateByInches($_POST['Height']);*/
+        header("Location: main.php?weight=".$num);
+        exit;
+		//echo "<div class = 'number'> The ideal weight is {$num} </div>";
+        break;
+    case 0:
+	$num=$person->CalculateByInches($_POST['Height']);
+    header("Location: main.php?weight=".$num);
+        exit;
+//echo "<div class = 'number'> The ideal weight is {$num} </div>";
+break;}
+	}
+
 }
 ?>
