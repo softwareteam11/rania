f2c411a60b101d457f0ad7ec07c771b0d52714c4
AmalNafe3 <amal.nafei@gmail.com>
2016-04-23 14:45:51 +0200

    Update controller.php

--- controller.php
+++ controller.php
@@ -8,6 +8,7 @@ class controller {
 	private static  $_instance=null;
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public static  function get_instance_controller  (    ){
+		
 								  if (!self::$_instance){
 									 self::$_instance = new controller (); 
 								  }							 
@@ -19,9 +20,14 @@ class controller {
 		/*$tracks=diet::get_instance_diet();*/
 	$d=new diet();
 	diet::show_diets();
+	//$d->dodo();
 		/*return $tracks_array ;*/
 	}
-
-    
+	
+	public function schedule(){
+	$d=new diet();
+	$results = diet::diet_schedule();
+	return $results;
+	}
 }
 ?>
